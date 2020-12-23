9d63b97f62a49dfa031456787073fc121bd53a3c
DrChinglish <zyd063@126.com>
2020-12-24 01:10:35 +0800

    小改

--- my-app/src/js/LoginIn.js
+++ my-app/src/js/LoginIn.js
@@ -53,6 +53,7 @@ class LoginIn extends Component {
                     })
                 } else if (this.state.type === "teacher" && this.state.isLogin) {
                     this.setState({
+                        flag: 4,
                     })
                 } else {
                     alert("用户名和密码错误!");
@@ -73,34 +74,34 @@ class LoginIn extends Component {
     }
 
     render(){
-        if(this.state.flag === 1){
+        if (this.state.flag === 1) {
             return (
                 <div>
                     <Title></Title>
-                    <div className = "Logo_Login" style={{float:'left'}}>
+                    <div className="Logo_Login" style={{ float: 'left' }}>
                         <Body></Body>
                     </div>
-                    <div className = "Info_Login" style={{float:'left'}}>
+                    <div className="Info_Login" style={{ float: 'left' }}>
                         <div>
                             <form>
                                 <div>
                                     <div>
-                                        <Input id = "text" type = "text" name = "用户名" placeholder = "用户名" ref = "name" onChange = {(e)=>this.GetUsername(e)}/>
+                                        <Input id="text" type="text" name="用户名" placeholder="用户名" ref="name" onChange={(e) => this.GetUsername(e)} />
                                     </div>
                                 </div>
                                 <div>
                                     <div>
-                                        <Input id = "text" type = "password" name = "密码" placeholder = "密码" ref = "code" onChange = {(e)=>this.GetCode(e)}/>
+                                        <Input id="text" type="password" name="密码" placeholder="密码" ref="code" onChange={(e) => this.GetCode(e)} />
                                     </div>
                                 </div>
                             </form>
                             <div>
-                                <Button onClick = {this.AppData}>
+                                <Button onClick={this.AppData}>
                                     登录
                                 </Button>
                             </div>
                             <div>
-                                <Button onClick = {this.Back}>
+                                <Button onClick={this.Back}>
                                     返回
                                 </Button>
                             </div>
@@ -108,10 +109,12 @@ class LoginIn extends Component {
                     </div>
                 </div>
             );
-        }else if(this.state.flag === 0){
-            return <Redirect to = {{pathname:'/'}} />
-        }else if(this.state.flag === 3){
-            return <Redirect to = {{pathname:'/User'}} />
+        } else if (this.state.flag === 0) {
+            return <Redirect to={{ pathname: '/' }} />
+        } else if (this.state.flag === 3) {
+            return <Redirect to={{ pathname: '/User' }} />
+        } else if (this.state.flag === 4) {
+            return <Redirect to={{ pathname: '/Teacher'}} />
         }
         
     }
